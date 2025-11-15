6df9bcf4c2eb7c3a19552751c2c35941adbdb103
Byiringiro Aloys <byiringiroaloys8@gmail.com>
2025-11-15 21:32:27 +0200

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by ALOYS on 11/15/25.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
