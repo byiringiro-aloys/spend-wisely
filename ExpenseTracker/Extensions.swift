9c85bab3b9b2d7d9edab8efbe61c1730a6fc90bd
Byiringiro Aloys <byiringiroaloys8@gmail.com>
2025-11-16 08:56:32 +0200

    Added custom colors and navigationView

---
+++ ExpenseTracker/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  ExpenseTracker
+//
+//  Created by ALOYS on 11/16/25.
+//
+
+import SwiftUI
+import Foundation
+
+extension Color{
+    static let appBackground = Color("Background")
+    static let AppIcon = Color("Icon")
+    static let appText = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
