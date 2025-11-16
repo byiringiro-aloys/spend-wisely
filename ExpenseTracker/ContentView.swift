ead70abb8669ab238d86ffde95fd3a9477fc1670
Byiringiro Aloys <byiringiroaloys8@gmail.com>
2025-11-16 09:50:41 +0200

    finished setting up the template

--- ExpenseTracker/ContentView.swift
+++ ExpenseTracker/ContentView.swift
@@ -9,18 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
             NavigationView(){
                 ScrollView{
+                    VStack(alignment: .leading, spacing: 24){
+                        //MARK: Title
+                        Text("Overview")
+                            .font(.title2)
+                            .bold()
+                        }
+                        .padding()
+                        .frame(maxWidth: .infinity)
                     
                 }
                 .background(Color.appBackground)
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar{
+                    ToolbarItem(placement: .navigationBarTrailing){
+                        Image(systemName: "bell.badge")
+                            .symbolRenderingMode(.palette)
+                            .foregroundStyle(Color.AppIcon,.primary)
+                    }
+                }
             }
             .navigationViewStyle(.automatic)
         }
-        .padding()
     }
-}
 
 #Preview {
     ContentView()
